2f318f7b2f35747d0eac4b7ed03e42225bf2137b
AMI2080 <AMI_2080@yahoo.com>
2023-09-05 04:31:15 +0300

    Init calculation functions

--- js/script.js
+++ js/script.js
@@ -1,84 +1,165 @@
+const maxLength = 13;
+const calculation = {
+  current: 'first',
+  first: '0',
+  operator: '',
+  second: '',
+  result: '',
+  displayFunction: '',
+  error: '',
+  resest() {
+    this.switch('first');
+    this.first = '0';
+    this.operator = '';
+    this.second = '';
+    this.result = '';
+    this.displayFunction = '';
+    this.error = '';
+    this.display();
+  },
+  switch(current) {
+    this.current = current;
+    document.querySelector('input#display').setAttribute('data-current', current);
+  },
+  display() {
+    if (this.current === 'first' || this.current === 'second' || this.current === 'result') {
+      let displayedNumber = '0';
+      if (this[this.current].length > maxLength) {
+        const ePosition = this[this.current].indexOf('e+');
+        if (ePosition > -1) {
+          const numberWithoutE = this[this.current].slice(0, ePosition);
+          const e = this[this.current].slice(ePosition);
+          displayedNumber = numberWithoutE.slice(0, maxLength - ePosition - e.length) + e;
+        } else {
+          displayedNumber = this[this.current].slice(0, maxLength);
+        }
+      }
+      document.getElementById('display').value = this[this.current].length > maxLength ? displayedNumber : this[this.current];
+    } else if (this.current === 'operator') {
+      document.getElementById('display').value = this[this.second] ?? 0;
+    } else if (this.current === 'error') {
+      document.getElementById('display').value = this.error;
+    }
+    document.querySelector('.display-function').textContent = this.displayFunction;
+  },
+  keyEvent(key) {
+    if (key !== null && key >= 0 && key <= 9 || key === '00' || key === '.') {
+      if (this.current === 'operator') {
+        this.switch('second')
+      };
+      if (this.current === 'result') {
+        this.resest();
+      };
+      if (this.current === 'first' || this.current === 'second') {
+        if (this[this.current].length < maxLength) {
+          if (Number(this[this.current]) === 0 && this[this.current].indexOf('.') === -1) {
+            this[this.current] = '';
+          }
+          if (key >= 0 && key <= 9) {
+            this[this.current] += `${key}`;
+          } else if (key == '.') {
+            if (this[this.current].indexOf('.') === -1) {
+              this[this.current] += (this[this.current].length === 0 ? '0' : '') + String(key);
+            }
+          }
+        }
+      }
+    } else if (key === '+' || key === '-' || key === '*' || key === '/') {
+      if (this.current !== 'error') {
+        if (this.current === 'first' || this.current === 'result') {
+          this.switch('operator');
+        } else if (this.current === 'second') {
+          this.calculate();
+          this.switch('operator');
+          this.second = '0';
+        }
+        this.second = '';
+        this.operator = key;
+        this.displayFunction = `${this.first} ${this.operator.replace('/', '÷').replace('*', '×')}`;
+      }
+    } else if (key === 'Escape') {
+      this.resest();
+    } else if (key === 'Backspace') {
+      if ((this.current === 'first' || this.current === 'second') && this[this.current].length > 1) {
+        this[this.current] = this[this.current].substring(0, this[this.current].length - 1);
+      } else if ((this.current === 'first' || this.current === 'second') && this[this.current].length === 1) {
+        this[this.current] = '0';
+      }
+    } else if (key === '=') {
+      if (this.first.length > 0 && this.operator.length > 0 && this.second.length === 0) {
+        this.second = `${this.first}`;
+      }
+      this.displayFunction = `${this.first} ${this.operator.replace('/', '÷').replace('*', '×')} ${this.second} =`;
+      this.calculate();
+    }
+    this.display();
+  },
+  calculate() {
+    this.error = '';
+    this.switch('result');
+    let operatorSign = '';
+    switch (this.operator) {
+      case '+':
+        operatorSign = 'add';
+        break;
+      case '-':
+        operatorSign = 'subtract';
+        break;
+      case '*':
+        operatorSign = 'multiply';
+        break;
+      case '/':
+        operatorSign = 'divide';
+        break;
+      default:
+        break;
+    }
+    if (this.second == 0 && this.operator === '/') {
+      this.result = '0';
+      this.error = 'Cannot divide by zero';
+      this.switch('error');
+    } else if (this.first.length > 0 && this.operator.length > 0 && this.second.length > 0) {
+      const result = bigDecimal[operatorSign](this.first, this.second, 30);
+      if (result.length > 300) {
+        this.error = 'Overflow';
+        this.switch('error');
+      } else {
+        this.result = `${result * 1}`;
+      }
+    } else if (this.first.length > 0 && this.operator.length === 0 && this.second.length === 0) {
+      this.result = `${this.first}`;
+    } else {
+      this.result = `${this.result.length > 0 ? this.result : 0}`;
+    }
+    if (this.current !== 'error') {
+      this.first = `${this.result.length > 0 ? this.result : 0}`;
+    }
+    this.display();
+  }
+};
 document.querySelector('.change-mode').addEventListener('click', function () {
   if (document.querySelector('body').classList.contains('dark-theme')) {
-    document.querySelector('body').classList.add("light-theme")
-    document.querySelector('body').classList.remove("dark-theme")
+    document.querySelector('body').classList.add("light-theme");
+    document.querySelector('body').classList.remove("dark-theme");
   } else {
-    document.querySelector('body').classList.add("dark-theme")
-    document.querySelector('body').classList.remove("light-theme")
+    document.querySelector('body').classList.add("dark-theme");
+    document.querySelector('body').classList.remove("light-theme");
   }
-})
-
+});
 document.addEventListener('keydown', function (event) {
-  // console.log(event.code);
-  if (event.code == 'Digit0' || event.code == 'Numpad0') {
-    click('num-0')
-  } else if (event.code == 'Digit1' || event.code == 'Numpad1') {
-    click('num-1')
-  } else if (event.code == 'Digit2' || event.code == 'Numpad2') {
-    click('num-2')
-  } else if (event.code == 'Digit3' || event.code == 'Numpad3') {
-    click('num-3')
-  } else if (event.code == 'Digit4' || event.code == 'Numpad4') {
-    click('num-4')
-  } else if (event.code == 'Digit5' || event.code == 'Numpad5') {
-    click('num-5')
-  } else if (event.code == 'Digit6' || event.code == 'Numpad6') {
-    click('num-6')
-  } else if (event.code == 'Digit7' || event.code == 'Numpad7') {
-    click('num-7')
-  } else if (event.code == 'Digit8' || event.code == 'Numpad8') {
-    click('num-8')
-  } else if (event.code == 'Digit9' || event.code == 'Numpad9') {
-    click('num-9')
-  } else if (event.code == 'NumpadAdd') {
-    click('plus')
-  } else if (event.code == 'NumpadDivide') {
-    click('div')
-  } else if (event.code == 'Escape') {
-    click('esc')
-  } else if (event.code == 'Backspace') {
-    click('backspace')
-  }
-})
-
+  const key = event.key
+    .replace('Enter', '=');
+  document.querySelector(`button[data-btn="${key}"]`)?.classList.add("clicking");
+});
 document.addEventListener('keyup', function (event) {
-  console.log(event.code);
-  if (event.code == 'Digit0' || event.code == 'Numpad0') {
-    clicked('num-0')
-  } else if (event.code == 'Digit1' || event.code == 'Numpad1') {
-    clicked('num-1')
-  } else if (event.code == 'Digit2' || event.code == 'Numpad2') {
-    clicked('num-2')
-  } else if (event.code == 'Digit3' || event.code == 'Numpad3') {
-    clicked('num-3')
-  } else if (event.code == 'Digit4' || event.code == 'Numpad4') {
-    clicked('num-4')
-  } else if (event.code == 'Digit5' || event.code == 'Numpad5') {
-    clicked('num-5')
-  } else if (event.code == 'Digit6' || event.code == 'Numpad6') {
-    clicked('num-6')
-  } else if (event.code == 'Digit7' || event.code == 'Numpad7') {
-    clicked('num-7')
-  } else if (event.code == 'Digit8' || event.code == 'Numpad8') {
-    clicked('num-8')
-  } else if (event.code == 'Digit9' || event.code == 'Numpad9') {
-    clicked('num-9')
-  } else if (event.code == 'NumpadAdd') {
-    clicked('plus')
-  } else if (event.code == 'NumpadDivide') {
-    clicked('div')
-  } else if (event.code == 'Escape') {
-    clicked('esc')
-  } else if (event.code == 'Backspace') {
-    clicked('backspace')
-  }
-})
-
-function click(buttonName) {
-  document.querySelector('button[name=' + buttonName + ']').classList.add("clicking")
-}
-
-function clicked(buttonName) {
-  document.querySelector('button[name=' + buttonName + ']').click()
-  document.querySelector('button[name=' + buttonName + ']').classList.remove("clicking")
-}
+  const key = event.key
+    .replace('Enter', '=');
+  calculation.keyEvent(key);
+  document.querySelector(`button[data-btn="${key}"]`)?.classList.remove("clicking");
+});
+document.querySelectorAll('button').forEach(function (button) {
+  button.addEventListener('click', function () {
+    const inputName = this.getAttribute('data-btn');
+    calculation.keyEvent(inputName);
+  });
+});
